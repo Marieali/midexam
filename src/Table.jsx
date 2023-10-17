0c3f180d34ef8ad10f65888acf04c913407766f9
Marieali <alimariya149@gmail.com>
2023-10-17 10:17:12 +0500

    Initial table setup with static data

---
+++ src/Table.jsx
@@ -0,0 +1,91 @@
+const Table = () => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>Name</th>
+          <th>Age</th>
+          <th>City</th>
+          <th>Occupation</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>John Doe</td>
+            <td>25</td>
+            <td>New York</td>
+            <td>Engineer</td>
+        </tr>
+
+        <tr>
+            <td>2</td>
+            <td>Jane Smith</td>
+            <td>30</td>
+            <td>San Francisco</td>
+            <td>Designer</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Bob Johnson</td>
+            <td>28</td>
+            <td>Chicago</td>
+            <td>Accountant</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Alice Brown</td>
+            <td>35</td>
+            <td>Los Angeles</td>
+            <td>Teacher</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Charlie Wilson</td>
+            <td>40</td>
+            <td>Houston</td>
+            <td>Doctor</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Eva Davis</td>
+            <td>22</td>
+            <td>Miami</td>
+            <td>Artist</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Frank Miller</td>
+            <td>32</td>
+            <td>Seattl</td>
+            <td>Software Engineer</td>
+        </tr>
+
+        <tr>
+            <td>8</td>
+            <td>Grace Taylor</td>
+            <td>45</td>
+            <td>Boston</td>
+            <td>Lawyer</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Henry Clark</td>
+            <td>28</td>
+            <td>Denver</td>
+            <td>Marketing Manager</td>
+        </tr>
+
+        <tr>
+            <td>10</td>
+            <td>Ivy Adams</td>
+            <td>33</td>
+            <td>Atlanta</td>
+            <td>Entrepreneur</td>
+        </tr>
+
+      </table>
+    </div>
+  );
+};
+
+export default Table;
